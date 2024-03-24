df195e99adb347ba58e99a0d335dfdf7d4dc66fe
Christian Gentry <christianwgentry@gmail.com>
2024-03-24 18:44:43 -0400

    Add files via upload

---
+++ pages/4_🤺_Individual_players.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+from st_pages import Page, show_pages, add_page_title
+
+
+st.set_page_config(page_icon=':soccer:', layout="centered")
+
+st.write("Nothing to see here yet, I'm cooking up something good here soon!✌️")
+
+###sidebar section
+
+st.sidebar.subheader('''''')
+
+st.sidebar.info('''Hey! Want to connect? |  [**LinkedIn**](https://www.linkedin.com/in/christian-wl-gentry/)
+                | [**Twitter**](https://twitter.com/_chocolatejuice?s=11)''')
+#########################
